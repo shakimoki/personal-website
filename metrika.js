bb0929cf39dd39e1a99907275f891457ef9259ca
Алексей Морозов <shakimoki1@gmail.com>
2026-04-15 20:27:47 +0300

    Add Yandex Metrika

---
+++ metrika.js
@@ -0,0 +1,25 @@
+(function(m, e, t, r, i, k, a) {
+    m[i] = m[i] || function() {
+        (m[i].a = m[i].a || []).push(arguments);
+    };
+    m[i].l = 1 * new Date();
+    for (var j = 0; j < e.scripts.length; j += 1) {
+        if (e.scripts[j].src === r) return;
+    }
+    k = e.createElement(t);
+    a = e.getElementsByTagName(t)[0];
+    k.async = 1;
+    k.src = r;
+    a.parentNode.insertBefore(k, a);
+})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=108566055', 'ym');
+
+ym(108566055, 'init', {
+    ssr: true,
+    webvisor: true,
+    clickmap: true,
+    ecommerce: 'dataLayer',
+    referrer: document.referrer,
+    url: location.href,
+    accurateTrackBounce: true,
+    trackLinks: true
+});
